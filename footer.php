064187529db98053be3c904e62e42907c93c97a9
Kwame Adaboh <kwameadaboh@gmail.com>
2020-02-29 20:24:47 +0000

    index.php kinda complete, just need pictures

---
+++ footer.php
@@ -0,0 +1,118 @@
+<footer class="footer_widgets">
+    <div class="footer_top">
+        <div class="container">
+            <div class="row">
+               <div class="col-lg-6">
+                    <div class="row">
+                       
+                        <div class="col-lg-7 col-md-7 col-sm-7">
+                            <div class="widgets_container contact_us">
+                               <div class="footer_logo">
+                                   <a href="index.html"><img src="images/logo2.png" alt=""></a>
+                               </div>
+                                <div class="footer_desc">
+                                    <p>We are a team of designers and developers that create high quality HTML Template, Woocommerce, Shopify Theme.</p>
+                                </div>
+
+                                <p><span>Address:</span> 49 Park Avenue, East 58st Street, Apt. 50 New York NY 10025</p>
+                                <p><span>Email:</span> <a target="_blank" href="https://hasthemes.com/contact-us/">https://hasthemes.com/contact-us/</a></p>
+                                <p><span>Phone:</span> <a href="tel:+1250367864587">+1 250 3678 64587</a></p>
+                            </div>
+                        </div>
+                        <div class="col-lg-5 col-md-5 col-sm-5">
+                            <div class="widgets_container widget_menu">
+                                <h3>Customer Service</h3>
+                                <div class="footer_menu">
+                                    <ul>
+                                        <li><a href="#">Affiliates</a></li>
+                                        <li><a href="about.html">About Us</a></li>
+                                        <li><a href="#">Delivery Information</a></li>
+                                        <li><a href="#">Privacy Policy</a></li>
+                                        <li><a href="#">Terms & Conditions</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 col-sm-4">
+                            <div class="widgets_container widget_menu">
+                                <h3>My Account</h3>
+                                <div class="footer_menu">
+                                    <ul>
+                                        <li><a href="my-account.html">My Account</a></li>
+                                        <li><a href="#">Order History</a></li>
+                                        <li><a href="wishlist.html">Wish List</a></li>
+                                        <li><a href="#">Gift Certificates</a></li>
+                                        <li><a href="#">Returns</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-8 col-md-8 col-sm-8">
+                            <div class="widgets_container widget_menu">
+                                <h3>Newsletter</h3>
+                                <div class="subscribe_form">
+                                    <form id="mc-form" class="mc-form footer-newsletter">
+                                        <input id="mc-email" type="email" autocomplete="off" placeholder="Enter you email address">
+                                        <button id="mc-submit"><i class="zmdi zmdi-email-open"></i></button>
+                                    </form>
+                                    <!-- mailchimp-alerts Start -->
+                                    <div class="mailchimp-alerts text-centre">
+                                        <div class="mailchimp-submitting"></div><!-- mailchimp-submitting end -->
+                                        <div class="mailchimp-success"></div><!-- mailchimp-success end -->
+                                        <div class="mailchimp-error"></div><!-- mailchimp-error end -->
+                                    </div><!-- mailchimp-alerts end -->
+                                </div>
+                                <div class="footer_social">
+                                    <h3>follow us on</h3>
+                                    <ul>
+                                        <li><a href="#"><i class="zmdi zmdi-facebook"></i></a></li>
+                                        <li><a href="#"><i class="zmdi zmdi-twitter"></i></a></li>
+                                        <li><a href="#"><i class="zmdi zmdi-rss"></i></a></li>
+                                        <li><a href="#"><i class="zmdi zmdi-youtube-play"></i></a></li>
+                                        <li><a href="#"><i class="zmdi zmdi-google-old"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="footer_bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="footer_bottom_container">
+                        <div class="footer_link">
+                            <ul>
+                                <li><a href="contact.html">Contact Us</a></li>
+                                <li><a href="#">Site Map</a></li>
+                                <li><a href="#">Specials</a></li>
+                                <li><a href="#">Returns</a></li>
+                            </ul>
+                        </div>
+                        <div class="copyright_area">
+                                <p>Copyright &copy; 2019 <a href="#">Presiden</a>  All Right Reserved.</p>
+                            </div>
+                        <div class="footer_payment">
+                            <ul>
+                                <li><a href="#"><img src="images/paypal.jpg" alt=""></a></li>
+                                <li><a href="#"><img src="images/paypal1.jpg" alt=""></a></li>
+                                <li><a href="#"><img src="images/paypal2.jpg" alt=""></a></li>
+                                <li><a href="#"><img src="images/paypal3.jpg" alt=""></a></li>
+                                <li><a href="#"><img src="images/paypal4.jpg" alt=""></a></li>
+                                <li><a href="#"><img src="images/paypal5.jpg" alt=""></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>   
+</footer>
---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+    <head>
+     
+      <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title>Albert Awuku Young </title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- Favicon -->
+      <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+      
+      <!-- CSS 
+      ========================= -->
+      <!--bootstrap min css-->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!--owl carousel min css-->
+      <link rel="stylesheet" href="css/owl.carousel.min.css">
+      <!--slick min css-->
+      <link rel="stylesheet" href="css/slick.css">
+      <!--magnific popup min css-->
+      <link rel="stylesheet" href="css/magnific-popup.css">
+      <!--font awesome css-->
+      <link rel="stylesheet" href="css/font.awesome.css">
+      <!--ionicons min css-->
+      <link rel="stylesheet" href="css/ionicons.min.css">
+      <!--material design min css-->
+      <link rel="stylesheet" href="css/material.design.min.css">
+      <!--animate css-->
+      <link rel="stylesheet" href="css/animate.css">
+      <!--jquery ui min css-->
+      <link rel="stylesheet" href="css/jquery-ui.min.css">
+      <!--slinky menu css-->
+      <link rel="stylesheet" href="css/slinky.menu.css">
+      <!--plugins css-->
+      <link rel="stylesheet" href="css/plugins.css">
+      
+      <!-- Main Style CSS -->
+      <link rel="stylesheet" href="css/style.css">
+      
+      <!--modernizr min js here-->
+      <script src="js/modernizr-3.7.1.min.js"></script>
+       
+        <title>
+        </title>
+    </head>
+
+    <body>
+     <?php 
+
+     include 'header.php';
+     ?>
+
+    <section class="slider_section">
+      <div class="slider_area slider_one_area owl-carousel">
+          <div class="single_slider d-flex align-items-center" data-bgimg="assets/img/slider/slider1.jpg">
+             <div class="container">
+                 <div class="row">
+                     <div class="col-lg-8 col-md-8 col-sm-7">
+                         <div class="slider_content content_left">
+                             <h1>Fashion Items</h1>
+                              <h2>Men's suits & shirts</h2>  
+                              <a class="button" href="shop.html">Read more</a>
+                          </div>
+                     </div>
+                 </div>
+             </div>
+              
+          </div>
+          <div class="single_slider single_slider2 d-flex align-items-center" data-bgimg="assets/img/slider/slider2.jpg">
+              <div class="container">
+                 <div class="row">
+                     <div class="col-lg-7 offset-lg-5 col-md-7 offset-md-5 col-sm-7 offset-sm-5">
+                         <div class="slider_content content_right">
+                              <h1> Bayern Lookbook</h1>
+                              <h2>30% of all Women's</h2>
+                              <a class="button" href="shop.html">Read more</a>
+                          </div>
+                     </div>
+                 </div>
+             </div>
+          </div>
+      </div>
+  </section>
+
+
+
+ <?php include 'footer.php' ?>
+
+
+
+
+
+
+
+    
+    
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <!--popper min js-->
+    <script src="js/popper.js"></script>
+    <!--bootstrap min js-->
+    <script src="js/bootstrap.min.js"></script>
+    <!--owl carousel min js-->
+    <script src="js/owl.carousel.min.js"></script>
+    <!--slick min js-->
+    <script src="js/slick.min.js"></script>
+    <!--magnific popup min js-->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <!--counterup min js-->
+    <script src="js/jquery.counterup.min.js"></script>
+    <!--jquery countdown min js-->
+    <script src="js/jquery.countdown.js"></script>
+    <!--jquery ui min js-->
+    <script src="js/jquery.ui.js"></script>
+    <!--jquery elevatezoom min js-->
+    <script src="js/jquery.elevatezoom.js"></script>
+    <!--isotope packaged min js-->
+    <script src="js/isotope.pkgd.min.js"></script>
+    <!--slinky menu js-->
+    <script src="js/slinky.menu.js"></script>
+    <!-- Plugins JS -->
+    <script src="js/plugins.js"></script>
+    
+    <!-- Main JS -->
+    <script src="js/main.js"></script>
+    </body>
+
+</html>
